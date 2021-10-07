62c5d7367b112a985734b15cdae024cde87995bd
Md. Rabiul Ali Sarker <sarkerrabi16@gmail.com>
2021-10-07 15:24:16 +0600

    initial design done

---
+++ app/src/main/java/com/jachai/jachaimart/ui/checkout/CheckoutViewModel.kt
@@ -0,0 +1,7 @@
+package com.jachai.jachaimart.ui.checkout
+
+import androidx.lifecycle.ViewModel
+
+class CheckoutViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/jachai/jachaimart/ui/shop/ShopViewModel.kt
@@ -0,0 +1,7 @@
+package com.jachai.jachaimart.ui.shop
+
+import androidx.lifecycle.ViewModel
+
+class ShopViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
