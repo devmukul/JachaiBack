e706fa4a54ee8ea062e53549c0e14cea690002fc
Md. Rabiul Ali Sarker <sarkerrabi16@gmail.com>
2021-10-19 10:39:46 +0600

    cart implementation done

--- app/src/main/java/com/jachai/jachaimart/ui/cart/CartViewModel.kt
+++ app/src/main/java/com/jachai/jachaimart/ui/cart/CartViewModel.kt
@@ -1,7 +1,55 @@
 package com.jachai.jachaimart.ui.cart
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.MutableLiveData
+import com.jachai.jachaimart.JachaiFoodApplication
+import com.jachai.jachaimart.model.order.ProductOrder
+import com.jachai.jachaimart.ui.base.BaseViewModel
 
-class CartViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class CartViewModel(application: Application) : BaseViewModel(application) {
+
+    var successShopDetailsLiveData = MutableLiveData<ProductOrder>()
+    var successProductOrderListLiveData = MutableLiveData<List<ProductOrder>>()
+
+
+    fun getOrderDetails() {
+        if (JachaiFoodApplication.mDatabase.daoAccess().getProductOrdersSize() > 0) {
+            successShopDetailsLiveData.postValue(
+                JachaiFoodApplication.mDatabase.daoAccess().getProductOrders()[0]
+            )
+        } else {
+
+        }
+    }
+
+    fun updateQuantity(item: ProductOrder?) {
+        if (item != null) {
+            item.quantity?.let {
+
+                if (it <= 0) {
+                    JachaiFoodApplication.mDatabase.daoAccess()
+                        .deleteCartProducts(listOf(item.product))
+
+
+                } else {
+                    JachaiFoodApplication.mDatabase.daoAccess()
+                        .updateProductQuantity(it, item.product)
+
+                }
+                geOrderList()
+
+
+            }
+
+        }
+
+    }
+
+    fun geOrderList() {
+        successProductOrderListLiveData.postValue(
+            JachaiFoodApplication.mDatabase.daoAccess().getProductOrders()
+        )
+    }
+
+
 }
