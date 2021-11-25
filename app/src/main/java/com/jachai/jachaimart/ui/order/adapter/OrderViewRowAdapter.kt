b11fc536070b7cd53fcf715c39c1c222947cd4e8
Md. Rabiul Ali Sarker <sarkerrabi16@gmail.com>
2021-11-25 14:32:59 +0600

    time issue solved

--- app/src/main/java/com/jachai/jachaimart/ui/order/adapter/OrderViewRowAdapter.kt
+++ app/src/main/java/com/jachai/jachaimart/ui/order/adapter/OrderViewRowAdapter.kt
@@ -1,7 +1,6 @@
 package com.jachai.jachaimart.ui.order.adapter
 
 import android.content.Context
-import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,6 +9,8 @@ import com.jachai.jachaimart.R
 import com.jachai.jachaimart.databinding.OrderRowBinding
 import com.jachai.jachaimart.model.order.history.Order
 import com.jachai.jachaimart.utils.constant.ApiConstants
+import java.text.SimpleDateFormat
+import java.util.*
 
 class OrderViewRowAdapter(
     private val context: Context,
@@ -27,26 +28,28 @@ class OrderViewRowAdapter(
                 if (order != null) {
                     orderId.text = order.orderId
                     shopName.text = order.shop?.name ?: "Shop "
-                    orderTime.text = order.createdAt
+                    orderTime.text = order.createdAt?.let { getDateFormatter(it) }
+
+
                     totalCostOfOrder.text = order.total.toString()
                     if (!isOnGoingOrder) {
                         tracOrder.visibility = View.GONE
                     }
                     orderStatus.text = order.status.toString()
-                    if (order.status.equals(ApiConstants.ORDER_CANCELLED)){
+                    if (order.status.equals(ApiConstants.ORDER_CANCELLED)) {
 //                        orderStatus.setBackgroundResource(R.color.failed)
 
                         orderStatus.setTextColor(view.resources.getColor(R.color.failed))
-                    }else if (order.status.equals(ApiConstants.ORDER_COMPLETED)
+                    } else if (order.status.equals(ApiConstants.ORDER_COMPLETED)
                         ||
                         order.status.equals(ApiConstants.ORDER_DELIVERED)
                         ||
-                        order.status.equals(ApiConstants.ORDER_REVIEWED)){
-
+                        order.status.equals(ApiConstants.ORDER_REVIEWED)
+                    ) {
 
 
                         orderStatus.setTextColor(view.resources.getColor(R.color.success))
-                    }else{
+                    } else {
                         orderStatus.setTextColor(view.resources.getColor(R.color.processing))
 
                     }
@@ -57,6 +60,14 @@ class OrderViewRowAdapter(
             }
 
 
+        }
+
+        private fun getDateFormatter(date: String): String {
+            val inputFormatter = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss")
+            inputFormatter.timeZone = TimeZone.getTimeZone("UTC")
+            val date = inputFormatter.parse(date)
+            val outFormatter = SimpleDateFormat("dd MMM yyyy h:mm a ")
+            return outFormatter.format(date)
         }
 
     }
@@ -71,7 +82,7 @@ class OrderViewRowAdapter(
         val data = list[position]
         holder.bind(context, data, isOnGoingOrder)
         holder.itemView.setOnClickListener {
-            interaction.onOrderSelected(data,isOnGoingOrder)
+            interaction.onOrderSelected(data, isOnGoingOrder)
         }
     }
 
@@ -85,8 +96,10 @@ class OrderViewRowAdapter(
         }
     }
 
+
     interface Interaction {
         fun onOrderSelected(order: Order?, isOnGoingOrder: Boolean)
     }
+
 
 }
