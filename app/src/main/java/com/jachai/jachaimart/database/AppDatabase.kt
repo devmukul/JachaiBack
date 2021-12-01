02a5bcac46b893a4b54579d69795350482a9568f
Md. Rabiul Ali Sarker <sarkerrabi16@gmail.com>
2021-12-01 14:13:38 +0600

    database version name changed

--- app/src/main/java/com/jachai/jachaimart/database/AppDatabase.kt
+++ app/src/main/java/com/jachai/jachaimart/database/AppDatabase.kt
@@ -26,7 +26,7 @@ import com.jachai.jachaimart.utils.constant.CommonConstants.DATABASE_NAME
         Shop::class,
         SearchHistoryItem::class
     ],
-    version = 2,
+    version = 12,
     exportSchema = false
 )
 @TypeConverters(Converters::class)
